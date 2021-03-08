0fd16b89ae3e2e5cfbc49e03dd0caad488577de3
bhosalepayal <bhosalepayal78@gmail.com>
2021-03-08 16:39:33 +0530

    Login ui complete

--- app/src/main/java/com/nbn/tgji/ui/LoginActivity.java
+++ app/src/main/java/com/nbn/tgji/ui/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.nbn.tgji.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void getOtp(View view) {
+        startActivity(new Intent(this, OtpVerification.class));
     }
 }
